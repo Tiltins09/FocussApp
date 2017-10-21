e0ce690167b16029eabc19831290e076d5c503ab
tiltins09 <martins.tiltins@gmail.com>
2017-10-21 17:53:31 +0300

    Pause/Resume function added

--- JS/FocusApp.js
+++ JS/FocusApp.js
@@ -1,26 +1,47 @@
+// Starting time
 var seconds = 0;
 var minutes = 0;
 
+
+// Timer function. Counts till 25 minutes then launches the notifications function
+
 function timer() {
+    var timer = document.getElementById("timer");
+
     if (seconds == 60) {
         minutes = minutes + 1;
         seconds = 0;
     }
 
-    var timer = document.getElementById("timer");
     timer.innerHTML = "<h1>Time: " + minutes + "." + seconds + "</h1>";
 
     if (minutes == 25) {
         timer.innerHTML = "25 minutes have passed";
-        clearInterval(mytimer);
-        write(seconds, minutes);
+        intervals.forEach(clearInterval);
         notification();
     }
     seconds = seconds + 1;
 }
 
-var mytimer = setInterval(timer, 1000);
-mytimer;
+var intervals = [];
+
+// Start the timer 
+
+function initiateTimer(pos) {
+    console.log(pos);
+    if (pos == "start") {
+        console.log("Resume called");
+        var mytimer = setInterval(timer, 10);
+        mytimer;
+        intervals.push(mytimer);
+    } else if (pos == "end") {
+        console.log("Pause called");
+        clearInterval(mytimer);
+    }
+}
+
+
+// Starts the blinking notification
 
 function notification() {
 
@@ -41,6 +62,20 @@ function notification() {
 
 }
 
-function end() {
-    window.location.reload();
+// Either pauses or resumes the timer
+
+function pause() {
+    var button = document.getElementById("pause");
+    if (button.innerText == "Pause") {
+        intervals.forEach(clearInterval);
+        initiateTimer("end");
+        button.innerText = "Resume";
+    } else if (button.innerText == "Resume") {
+        initiateTimer("start");
+        button.innerText = "Pause";
+    }
 }
+
+// Starts the timer when app is loaded
+
+initiateTimer("start");
